dd8b620361c5443d638653bad6dd7d09a1d3788a
IT24100562 <it24100562@my.sliit.lk>
2026-04-24 14:44:27 +0530

    Initial commit: restaurant table reservation system

---
+++ src/main/java/com/restaurant/tablemanagement/TableManagementApplication.java
@@ -0,0 +1,12 @@
+package com.restaurant.tablemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableManagementApplication.class, args);
+    }
+}
