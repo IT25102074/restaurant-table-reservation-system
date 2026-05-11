0f85452e237a4a8f574bf9feb0a9a803b26a5d02
IT25101496 <it25101496@my.sliit.lk>
2026-05-11 16:01:07 +0530

    [ADD] User management module

---
+++ src/main/java/com/restaurant/reservation/model/User.java
@@ -0,0 +1,15 @@
+package com.reservesmart.model;
+
+public class User {
+
+    private Long userId;
+    private String fullName;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
