8b7422b9081eb5a4d95e24be1b44b7929875cc95
Kowshika <it25102504@my.sliit.lk>
2026-05-15 07:25:57 +0530

    Table management module

--- src/main/java/com/reservesmart/dto/TableDTO.java
+++ src/main/java/com/reservesmart/dto/TableDTO.java
@@ -1,4 +1,15 @@
 package com.reservesmart.dto;
 
+import lombok.Data;
+
+/**
+ * DTO for transferring RestaurantTable data to views.
+ */
+@Data
 public class TableDTO {
+    private Long tableId;
+    private String tableNumber;
+    private int capacity;
+    private String location;
+    private String status;
 }
